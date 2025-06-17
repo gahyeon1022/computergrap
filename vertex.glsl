4d9569d08f09b2716493dbc72e9ba6eb7a8edc5d
gahyeon1022 <148620285+gahyeon1022@users.noreply.github.com>
2025-06-18 06:49:10 +0900

    Update vertex.glsl

--- vertex.glsl
+++ vertex.glsl
@@ -3,19 +3,19 @@
 attribute vec3 a_position;
 attribute vec3 a_normal;
 
-uniform mat4 u_PVM;
 uniform mat4 u_model_matrix;
 uniform mat3 u_normal_matrix;
+uniform mat4 u_PVM;
 
-varying vec3 v_position;   // fragment에서 사용할 position (world coord)
-varying vec3 v_normal;     // fragment에서 사용할 normal (world coord)
+varying vec3 v_position;
+varying vec3 v_normal;
 
 void main()
 {
-    // 최종 좌표
-    gl_Position = u_PVM * vec4(a_position, 1.0);
-
-    // 조명 계산용 데이터 전달
-    v_position = (u_model_matrix * vec4(a_position, 1.0)).xyz;
+    // 월드 좌표계로 변환된 위치와 노말
+    vec4 position_wc = u_model_matrix * vec4(a_position, 1.0);
+    v_position = position_wc.xyz;
     v_normal = normalize(u_normal_matrix * a_normal);
+
+    gl_Position = u_PVM * vec4(a_position, 1.0);
 }
